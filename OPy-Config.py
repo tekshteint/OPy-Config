5ebaf48050ef624c5cfe6472bad93c763536302b
Tom Ekshtein <tekshteint@gmail.com>
2023-04-13 19:11:22 -0700

    Cruise Control Logging Trigger
    Added functionality to enable logging via turning on the cruise control switch. Also added a trigger to only enable logging once RPMs are above 500 for more reliable logs.
    
    CSV had some minor changes so updates were added here as well.

--- OPy-Config.py
+++ OPy-Config.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 def csvRead() -> None:
     with open("opconfig_ncmx5_obd_pids_master.csv","r") as file:
@@ -7,8 +8,9 @@ def csvRead() -> None:
             if row[0]=="x":
                 linesToUse.append(row)
                 
-def textParse(memAddr: str) -> None:
-    with open("logcfg.txt", "w") as output:
+def textParse(memAddr: str, cruiseSwitch: bool) -> None:
+    filePath = os.getcwd() + os.sep + "logcfg.txt"
+    with open(filePath, "w+") as output:
         if memAddr != "":
             output.writelines([";-----[ channel setup   ]-----\n",
                         "\n",
@@ -41,6 +43,50 @@ def textParse(memAddr: str) -> None:
                                f"offsetbits = {col[7]}\n",
                                f"scalingrpn = {col[8]}\n\n"
             ])
+            
+        if cruiseSwitch:
+            output.writelines(["paramname=CruiseLight\n",
+                               ";description = Cruise Control Trigger\n",
+                               "mode = 0x22\n",
+                               "paramid = 0xA216\n",
+                               "databits = 16\n",
+                               "scalingrpn = x,0.000076293945,*\n",
+                               "sampgroup = 1\n",
+                               ";scaling = ((A*256)+B)*0.000076293945\n",
+                               "isvisible = 0\n",
+                               "units = V\n",
+                               "priority = 2\n\n",
+                               ";-------------triggers---------------\n",
+                               "; only log when the engine is running\n",
+                               "conditionrpn = RPM,500,>,&&\n",
+                               "action = start\n\n",
+                               "conditionrpn = RPM,0,==,&&\n",
+                               "action = stop\n\n",
+                               "conditionrpn = CruiseLight,1,==\n",
+                               "action = start\n"
+                               "conditionrpn = CruiseLight,0,==\n"
+                               "action = stop\n\n"
+                               ])
+        else:
+            output.writelines([";paramname=CruiseLight\n",
+                               ";description = Cruise Control Trigger\n",
+                               ";mode = 0x22\n",
+                               ";paramid = 0xA216\n",
+                               ";databits = 16\n",
+                               ";scalingrpn = x,0.000076293945,*\n",
+                               ";sampgroup = 1\n",
+                               ";scaling = ((A*256)+B)*0.000076293945\n",
+                               ";isvisible = 0\n",
+                               ";units = V\n",
+                               ";priority = 2\n\n",
+                               ";-------------triggers---------------\n",
+                               "; only log when the engine is running\n",
+                               "conditionrpn = RPM,500,>,&&\n",
+                               "action = start\n\n",
+                               "conditionrpn = RPM,0,==,&&\n",
+                               "action = stop\n\n"
+                               ])            
+        
 
 def getMemAddr() -> str:
     memAddr = input("Input memory address for logging uncorrected load.\nIf you wish to not log uncorrect load just press enter.\n")
@@ -49,9 +95,18 @@ def getMemAddr() -> str:
     else:
         return memAddr
     
+def getCruiseCtrl() -> bool:
+    print("\nWould like to enable logging only when cruise control is enabled? y/n:")
+    cCtrl = input()
+    if cCtrl.strip().lower() == "y":
+        return True
+    else:
+        return False
+    
 
 if __name__ == "__main__":
     linesToUse=[]
     memAddr = getMemAddr()
+    cruiseCtrl = getCruiseCtrl()
     csvRead()
-    textParse(memAddr)
+    textParse(memAddr, cruiseCtrl)
